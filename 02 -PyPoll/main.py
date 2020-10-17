e7be697af8be2f70368ef12ca74562acc4a4f86d
storm.breaker <67917417+hillarymandich@users.noreply.github.com>
2020-10-16 21:35:32 -0400

    Open file, define lists, total votes

--- 02 -PyPoll/main.py
+++ 02 -PyPoll/main.py
@@ -8,10 +8,27 @@ import os
 import csv
 import sys
 
-
 #identify path to elecation csv file,read csv file
-election_data =os.path.join("01 - Resouces", "election_data.csv")
+election_data =os.path.join("02 -PyPoll", "01 - Resouces", "election_data.csv")
+
+#create list for columns in csv file
+voterID = [] 
+county = []
+candidate = [] 
+
+
 with open(election_data,'r',encoding ='utf-8') as input_file:
     csvreader = csv.reader(input_file)
 
-    next(csvreader)
+    next(csvreader) #skip header
+
+    #create list 
+    for r in csvreader:
+        voterID.append(r[0])
+        county.append(r[1])
+        candidate.append(r[2])
+
+#total number of votes using len
+#totalvotes = 
+
+print('Total Votes:', len(voterID))
